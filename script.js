90ba208fdb5d00250407024de76ec8e1cc50fb83
Tyler <tylermdespain@gmail.com>
2016-01-20 11:08:33 -0700

    Click cats Premium
    changed to include an MVC (model, view, controller) organizational
    structure

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-var Cat = function (id, clickID, name, pic, picID, count, countID) {
+$(function() {
+// model
+var Cat = function (id, buttonID, name, pic, picID, count, countID) {
 	this.id = id;
-	this.clickID = clickID;
+	this.buttonID = buttonID;
 	this.name = name;
 	this.pic = pic;
 	this.picID = picID;
 	this.count = count;
 	this.countID = countID;
 };
-
-Cat.prototype.countIncrement = function () {
-	countsSel[this.value].text(count);
-};
-
-Cat.prototype.addCat = function () {
-	$('body').append('<div id="' + this.id + '" value="' + this.value + '"><h1 id="' + this.clickID + '" class="blue">' + this.name + '</h1></div>');
-	click = $('#' + this.clickID);
-	catDiv = $('#' + this.id);
-	catDiv.append('<div class=hide id="' + this.name + '"><h2>Count: <span id="' + this.countID + '">' + this.count + '</span></h2></div>')
-	countID = $('#' + this.countID);
-	hiddenDiv = $('#' + this.name);
-	hiddenDiv.append('<img id="'+ this.picID + '" src="' + this.pic + '"></div>');
-	catPic = $('#' + this.picID);
-
-	click.on('click', (function (hiddenDiv) {
-		return function() {
-			hiddenDiv.toggleClass('hide');
-		};
-	})(hiddenDiv));
-
-	catPic.on('click', (function (countID, cat) {
-		return function() {
-			cat.count++;
-
-			countID.text(cat.count);
-		}
-	})(countID, this));
-}
 
 var catOne = new Cat('catOne', 'clickOne', 'Mittens', 'images/cat.jpg', 'catOnePic', 0, 'countOne');
 var catTwo = new Cat('catTwo', 'clickTwo', 'Scruffie', 'images/catTwo.jpg', 'catTwoPic', 0, 'countTwo');
@@ -45,24 +18,49 @@ var catFive = new Cat('catFive', 'clickFive', 'Snowball', 'images/catFive.jpg', 
 
 var cats = [catOne, catTwo, catThree, catFour, catFive];
 
-catOne.addCat();
-catTwo.addCat();
-catThree.addCat();
-catFour.addCat();
-catFive.addCat();
+// octopus
+var octopus = {
+	init: function() {
+		viewOne.addButtons();
+	},
+	showCat: function(cat) {
+		$('.currentCat').remove();
 
+		$('body').append('<div id="' + cat.id + '" class="currentCat"><h1 id="' + cat.buttonID + '" class="blue">' + cat.name + '</h1></div>');
+		click = $('#' + cat.buttonID);
+		catDiv = $('#' + cat.id);
+		catDiv.append('<div id="' + cat.name + '"><h2>Count: <span id="' + cat.countID + '">' + cat.count + '</span></h2></div>')
+		countID = $('#' + cat.countID);
+		imageDiv = $('#' + cat.name);
+		imageDiv.append('<img id="'+ cat.picID + '" src="' + cat.pic + '"></div>');
+		catPic = $('#' + cat.picID);
 
-// <section>
-// 		<div>
-// 			<header id='clicks'>
-// 				<h1>Clicks: <span id='clickCountOne'>0</span></h1>
-// 				<p> How many times can you click on this cat? </p>
-// 			</header>
-// 		</div>
-// 		<div id="catOne">
-// 			<h2 id='catNameOne'><h2>
-// 			<picture>
-// 				<img id='catPicOne' class='img' src='images/cat.jpg' alt='Cat Picture'  value='0'>
-// 			</picture>
-// 		</div>
-// 	</section>
+		catPic.on('click', (function (countID, cat) {
+			return function() {
+				cat.count++;
+
+				countID.text(cat.count);
+			}
+		})(countID, cat));
+		}
+};
+// view
+var viewOne = {
+	addButtons: function() {
+		for (var i=0; i < cats.length; i++) {
+			$('#catList').append('<li><button class="' + cats[i].buttonID + '"> ' + cats[i].name + '</button></li>')
+
+			var button = $('.' + cats[i].buttonID);
+
+			button.on('click', (function(cat){
+				return function() {
+					octopus.showCat(cat);
+				}
+			})(cats[i]));
+		};
+
+	},
+};
+
+octopus.init();
+});
